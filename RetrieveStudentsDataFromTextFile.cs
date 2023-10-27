725e5f8e1bec54db8764b7e20504301eda8ed085
SHUBHA GURUDEV <89259692+Shubhagurudev@users.noreply.github.com>
2023-10-27 16:24:22 +0530

    code

---
+++ RetrieveStudentsDataFromTextFile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentsDataRetrievalFromTextFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream("C:\\Users\\shubh\\source\\repos\\StudentDataTextFile.txt", FileMode.Open, FileAccess.Read);
+
+
+            string data = File.ReadAllText("C:\\Users\\shubh\\source\\repos\\StudentDataTextFile.txt");
+            Console.WriteLine(data);
+
+            Console.ReadLine();
+        }
+    }
+}
